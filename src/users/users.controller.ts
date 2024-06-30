ccde8675a51f1d79579b3a6d8d1a68eefec75932
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-06-30 13:48:37 +0300

    Finish create and get all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    const user = await this.userService.create(body.email, body.password);
+
+    return user;
+  }
+
+  @Get('/')
+  async getAllUsers() {
+    const users = this.userService.getAll();
+
+    return users;
+  }
+}
