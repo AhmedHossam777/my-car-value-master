eb528a5564341e5f62a4e4e073752b42ec6c58c0
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-07-08 01:44:22 +0300

    add custom data serlaization

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,27 @@
+import { NestInterceptor, ExecutionContext, CallHandler, UseInterceptors } from '@nestjs/common';
+
+import { plainToClass } from 'class-transformer';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export function Serialize (dto: any){
+  return UseInterceptors(new SerializeInterceptor(dto))
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    return next.handle().pipe(
+      // data : is the data that we send back in the response
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
