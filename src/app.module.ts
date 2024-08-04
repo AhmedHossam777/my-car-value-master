445cca19411379381fb6326b750c85308ef4bea8
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-08-04 07:57:19 +0300

    add configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,30 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
-    }),
-    UsersModule,
-    ReportsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: `.env.${process.env.NODE_ENV}`,
+		}),
+		TypeOrmModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => {
+				return {
+					type: 'sqlite',
+					database: configService.get<string>('DB_NAME'),
+					synchronize: true,
+					entities: [User, Report],
+				};
+			},
+		}),
+		UsersModule,
+		ReportsModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -1,24 +1,23 @@
 import {
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-  Injectable,
+	CallHandler,
+	ExecutionContext,
+	Injectable,
+	NestInterceptor,
 } from '@nestjs/common';
 
 import { UsersService } from '../users.service';
 
 @Injectable()
 export class CurrentUserInterceptor implements NestInterceptor {
-  constructor(private userService: UsersService) {}
-  async intercept(context: ExecutionContext, next: CallHandler<any>) {
-    const request = context.switchToHttp().getRequest();
-    const { userId } = request.session || {};
+	constructor(private userService: UsersService) {}
+	async intercept(context: ExecutionContext, next: CallHandler<any>) {
+		const request = context.switchToHttp().getRequest();
+		const { userId } = request.session || {};
 
-    if (userId) {
-      const user = await this.userService.findOne(userId);
-      request.currentUser = user;
-    }
+		if (userId) {
+			request.currentUser = await this.userService.findOne(userId);
+		}
 
-    return next.handle();
-  }
+		return next.handle();
+	}
 }
